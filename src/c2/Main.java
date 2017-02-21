24847a4d164f2e5ad7ccce7a8ef92e199a8b8edd
Juanlu <jlljunior_90@hotmail.com>
2017-02-21 23:53:02 +0100

    proyecto añadido

---
+++ src/c2/Main.java
@@ -0,0 +1,18 @@
+package c2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		ThreadEjemplo th1 =new ThreadEjemplo();
+		 th1.start();
+		 ThreadEjemplo th2 =new ThreadEjemplo();
+		 th2.start();
+		 ThreadEjemplo th3 =new ThreadEjemplo();
+		 th3.start();
+		 ThreadEjemplo th4 =new ThreadEjemplo();
+		 th4.start();
+	    
+
+	}
+
+}
